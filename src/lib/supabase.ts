d637ecaf2e20eb127b7c5a383200c47ccef9c434
crazyeye6 <regna007@protonmail.com>
2026-04-17 18:24:49 +0100

    Fix Supabase connection in build

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://peuwvmctzmlhbtgplyth.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBldXd2bWN0em1saGJ0Z3BseXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MzgwMDIsImV4cCI6MjA5MjAxNDAwMn0.nCxMGSJcFACnTdPzb2pNENViQvmrqeLrjF0sE-KVXNI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
